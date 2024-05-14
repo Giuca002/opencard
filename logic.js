692b4c3f2b0ff28886e52881816b04a8914f5a8a
Giuca002 <85449670+Giuca002@users.noreply.github.com>
2024-05-13 22:59:50 -0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -128,14 +128,6 @@ let touchStartFired = false;
 
 var loadElement = document.getElementById("loadbtn");
 
-loadElement.addEventListener('touchstart', function(event) {
-  // Prevent the default action to avoid double click/tap
-  event.preventDefault();
-  // Call your flipCard function here
-  openModal()
-  
-      
-});
 
 
 flipCardElement.addEventListener('touchstart', function(event) {
